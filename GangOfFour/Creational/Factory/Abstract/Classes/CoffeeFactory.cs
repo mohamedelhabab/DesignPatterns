97e9a0c9cb9f04ad69fb89996845cacddec0da8c
Mohamed Elhabbab <dev.mohamed.elhabab@gmail.com>
2019-11-02 00:48:27 +0200

    Abstract Factory For Hot Drinks Machine
     Steps :-
    ---------
     1:- Build IHotDrink Interface And Implement It Using Available Drinks
    
    2:- Build IHotDrinkFactory Interface And Implement It Using Drinks Factories
    
    3:- Build HotDrinkMachineClass To Store The Mapping Between Drinks And Its Factories Using Reflection Then Return The Right DrinkFactory To Be Used .

---
+++ GangOfFour/Creational/Factory/Abstract/Classes/CoffeeFactory.cs
@@ -0,0 +1,19 @@
+﻿using DesignPatterns.GangOfFour.Creational.Factory.Abstract.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.GangOfFour.Creational.Factory.Abstract.Classes
+{
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine("Preparing Coffee Right Now !");
+
+            return new Coffee();
+        }
+    }
+}
---
+++ GangOfFour/Creational/Factory/Abstract/Interfaces/IHotDrink.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.GangOfFour.Creational.Factory.Abstract.Interfaces
+{
+    //First :- Build This Interface And Implement It Using Available Drinks
+    public interface IHotDrink
+    {
+        void Consume();
+    }
+}
